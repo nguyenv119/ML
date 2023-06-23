2af29bc7763dfd3edc02a1baacdfa2bc8be78569
Long Nguyen <nguyenv@brandeis.edu>
2023-06-22 20:58:00 -0700

    init practice files

---
+++ Pytorch/practice/practice.py
@@ -0,0 +1,7 @@
+import torch
+import pandas as pd
+import matplotlib as plt
+
+print("Torch: " + str(torch.__version__))
+print("Pandas: " + str(pd.__version__))
+print("MatPLotLib: " + str(plt.__version__))
